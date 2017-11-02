086f9921c2b2b8bdc6826919920d596f449fa5e6
Ayesha <ayesha@sdots.local>
2017-11-02 15:22:39 +0500

    table with json parsing as well as an interface

--- src/app/item.component.ts
+++ src/app/item.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { Car }  from './car';
+import 'rxjs/add/observable/of';
+import {DataSource} from '@angular/cdk/collections';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-item',
@@ -9,6 +12,45 @@ import { Component } from '@angular/core';
 
 export class ItemComponent {
 
+  dataSourceHero: any;
+  model1 = new Car(18, 'Toyota');
+  model2 = new Car(19, 'BMW');
+  model3 = new Car(20, 'Volvo');
+  array: any[];
+  displayedColumns = ['id', 'name'];
+  json: string;
 
+  constructor(){
+    this.array =  [this.model1, this.model2, this.model3];
+    this.json=JSON.stringify(this.array);
+    //this.dataSourceHero = new HeroDataSource(this.array);
+    this.dataSourceHero = new HeroDataSource(this.json);
+  }
+}
 
+export class HeroDataSource extends DataSource<any> {
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  json: string;
+
+  localCars: Car[]
+  /*constructor(private importedCar: Car[]){
+    super();
+    this.localCar = importedCar;
+    /!*
+     this.json = JSON.stringify(data);
+     this.ele = JSON.parse(this.json);*!/
+
+  }*/
+  constructor(private importedCars: string){
+    super();
+    this.localCars = JSON.parse(importedCars);
+
+  }
+  connect(): Observable<Car[]> {
+
+    /*console.log(JSON.stringify(data));*/
+    return Observable.of(this.localCars);
+  }
+
+  disconnect() {}
 }
